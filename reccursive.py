37f73e1f4dc847f89cff4d67ac0c11ab96fc0e81
jeswin01roy <135967640+jeswin01roy@users.noreply.github.com>
2026-07-01 20:28:38 +0530

    Update reccursive.py

--- reccursive.py
+++ reccursive.py
@@ -1,52 +1,40 @@
-# #reccursive function to print 10-0
+ #reccursive function to print 10-0
 
-# def countzero(n):
-#     print(n)
-#     if n==0:
-#         return 0
+ def countzero(n):
+     print(n)
+     if n==0:
+         return 0
     
-#     return countzero(n-1)
+     return countzero(n-1)
 
-# countzero(10)
+ countzero(10)
 
-# # reccursive function for factorial
-# def fact(n):
-#     if(n==0 or n==1):
-#         return 1
-#     else:
-#         return n*fact(n-1)
+ # reccursive function for factorial
+ def fact(n):
+     if(n==0 or n==1):
+         return 1
+     else:
+         return n*fact(n-1)
     
 
-# #sum of n numbers
-# def sum(n):
-#     if(n==0):
-#         return 0
-#     else:
-#         return n+sum(n-1)
+#sum of n numbers
+def sum(n):
+    if(n==0):
+        return 0
+    else:
+        return n+sum(n-1)
 
-# def main():
-#     while(True):
-#         print("\t\t factorial or sum usingrecursive")
-#         num= int(input("enter the range:"))
-#         choice= int(input("select the choice \n1.factorial\n2.sum of range\n3.stop\n choice: "))
-#         if(choice==1): 
-#             print(f"\nthe factorial of {num} is:",fact(num))
-#         elif(choice==2):   
-#             print(f"\nthe sum of  zero to {num} is:",sum(num))
-#         elif(choice==3):
-#             break
-#         else:
-#             print("invalid entry")
-# main()
-
-#scope of the variable
-
-name="david"
-def myname():
-    name="sreeja"
-    def nickname():
-        name="yazil"
-        return name
-    name=nickname()
-    return name
-print(myname())
+def main():
+    while(True):
+        print("\t\t factorial or sum usingrecursive")
+        num= int(input("enter the range:"))
+        choice= int(input("select the choice \n1.factorial\n2.sum of range\n3.stop\n choice: "))
+        if(choice==1): 
+            print(f"\nthe factorial of {num} is:",fact(num))
+        elif(choice==2):   
+            print(f"\nthe sum of  zero to {num} is:",sum(num))
+        elif(choice==3):
+            break
+        else:
+            print("invalid entry")
+main()
